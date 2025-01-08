b1963e8958d81a12bcbcca9cb041ea9cab18b876
Surya Prakash <anapalasuryaprakash8@gmail.com>
2025-01-08 11:56:28 +0530

    Loops and Functions

---
+++ function.js
@@ -0,0 +1,13 @@
+// function myfunc(){
+//     console.log("Hello");//function declaration
+// }
+// myfunc() //function calling;
+class A{
+ add(a,b){
+    console.log(a+b);
+}
+
+
+}
+let z=new A();
+z.add(5,3)
---
+++ looping.js
@@ -0,0 +1,28 @@
+// for(let i=0;i<5;i++){
+//     console.log("Hello World");
+// }
+// for(let i=0;i<10;i++){
+//     if(i%2==0) console.log("even")
+//         else console.log("odd")
+// }
+let n=4;
+let factorial=1;
+// if(n==0) factorial=1;
+// else{
+
+// for(let j=n;j>=1;j--){
+//     factorial=factorial*j;
+// }} 
+// console.log(factorial);
+// let i=1;
+// while(i<=n){
+//     factorial=factorial*i;
+//     i++;
+// }
+// console.log(factorial);
+let i=1;
+let I=2;
+do{
+console.log(i);
+i++;
+}while(i>=5);
