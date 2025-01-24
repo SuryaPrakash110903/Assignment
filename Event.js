f2145ffd17692bef9f374c93f41fedaf4a9b4766
Surya Prakash <anapalasuryaprakash8@gmail.com>
2025-01-24 11:46:14 +0530

    24/01/2025

---
+++ Event.js
@@ -0,0 +1,8 @@
+let btn=document.getElementsByClassName("btn")[0];
+btn.addEventListener("click",()=>{
+    document.body.style.backgroundColor="aqua"
+})
+let hover=document.getElementsByClassName('hover')[0];
+hover.addEventListener("mouseover",()=>{
+     document.body.style.backgroundColor="yellow"
+})
